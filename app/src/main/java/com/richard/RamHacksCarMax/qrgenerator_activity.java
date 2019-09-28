1a0121fb8520191cf4d7c6f91fa0e431822d498f
stitzrp <stitzrp@gmail.com>
2019-09-28 17:31:23 -0400

    fixed UI scaling

---
+++ app/src/main/java/com/richard/RamHacksCarMax/qrgenerator_activity.java
@@ -0,0 +1,17 @@
+package com.richard.RamHacksCarMax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class qrgenerator_activity extends AppCompatActivity {
+
+    private Button qr_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrgenerator_activity);
+    }
+}
