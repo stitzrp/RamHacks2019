59e70a6394671a355a6fd1587f234415bde5e562
NicolasMarfurtLevy <43423053+NicolasMarfurtLevy@users.noreply.github.com>
2019-09-28 16:00:16 -0400

    added buttons for main

--- app/src/main/java/com/richard/RamHacksCarMax/MainActivity.java
+++ app/src/main/java/com/richard/RamHacksCarMax/MainActivity.java
@@ -2,7 +2,12 @@ package com.richard.RamHacksCarMax;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.*;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void openCam(View view){
+        checkCameraHardware(getApplicationContext());
+            
+        }
+
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
 }
+
+
